53ecb685063d482937da8821952fc6bc76590e73
Linus Hoppe <hoppelinus@web.de>
2021-05-08 22:25:18 +0200

    PinMode

--- main/main.ino
+++ main/main.ino
@@ -4,7 +4,7 @@ int intervals = (AirValue - WaterValue)/3;
 int soilMoistureValue = 0;
 
 void setup() {
-  Serial.begin(9600); // serial port setup
+  Serial.begin(115200); // serial port setup
   pinMode(5, OUTPUT); // GREEN
   pinMode(0, OUTPUT); // BLUE 
   pinMode(4, OUTPUT); // RED
@@ -13,6 +13,7 @@ void setup() {
 
 void loop() {
   soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
+  Serial.println("Das ist der Wert" + String(soilMoistureValue));
   if(soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals)) { 
       Serial.println("Very Wet");
       digitalWrite(5, LOW);
@@ -34,3 +35,22 @@ void loop() {
   delay(100);
   digitalWrite(5, HIGH);
 }
+
+
+
+/*          RED
+ *          Serial.println("Dry");
+            digitalWrite(5, LOW);
+            digitalWrite(4, LOW);
+            digitalWrite(0, HIGH);
+            GREEN
+            Serial.println("Wet");
+            digitalWrite(4, LOW);
+            digitalWrite(0, LOW);
+            digitalWrite(5, HIGH);
+            BLUE
+            Serial.println("Very Wet");
+            digitalWrite(5, LOW);
+            digitalWrite(0, LOW);
+            digitalWrite(4, HIGH);
+*/
