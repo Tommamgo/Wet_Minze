5edc7c83888640c6793436b79c6e6a27bdc73361
Linus Hoppe <hoppelinus@web.de>
2021-05-08 22:35:25 +0200

    Lukas machte jetzt alles kaputt

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,4 @@
+
 const int AirValue = 520;   
 const int WaterValue = 260;  
 int intervals = (AirValue - WaterValue)/3;   
@@ -12,30 +13,29 @@ void setup() {
 }
 
 void loop() {
-  soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
-  Serial.println("Das ist der Wert" + String(soilMoistureValue));
-  if(soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals)) { 
-      Serial.println("Very Wet");
-      digitalWrite(5, LOW);
-      digitalWrite(0, LOW);
-      digitalWrite(4, HIGH);
-       
-    } else if(soilMoistureValue > (WaterValue + intervals) && soilMoistureValue < (AirValue - intervals)){
-         Serial.println("Wet");
-        digitalWrite(4, LOW);
-        digitalWrite(0, LOW);
-        digitalWrite(5, HIGH);
-         
-      } else if(soilMoistureValue < AirValue && soilMoistureValue > (AirValue - intervals)){
-            Serial.println("Dry");
+soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
+Serial.println("Hallo ich bin Lukas und dumm und ein HS: " + String(soilMoistureValue));
+if(soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals)) {
+  Serial.println("Very Wet");
+            digitalWrite(5, LOW);
+            digitalWrite(0, LOW);
+            digitalWrite(4, HIGH);
+  } else if(soilMoistureValue > (WaterValue + intervals) && soilMoistureValue < (AirValue - intervals))
+{
+  Serial.println("Wet");
+            digitalWrite(4, LOW);
+            digitalWrite(0, LOW);
+            digitalWrite(5, HIGH);
+}
+else if(soilMoistureValue < AirValue && soilMoistureValue > (AirValue - intervals))
+{
+  Serial.println("Dry");
             digitalWrite(5, LOW);
             digitalWrite(4, LOW);
             digitalWrite(0, HIGH);
-          }
-  delay(100);
-  digitalWrite(5, HIGH);
 }
-
+delay(100);
+}
 
 
 /*          RED
